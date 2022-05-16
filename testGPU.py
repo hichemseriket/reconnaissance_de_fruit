c4b67b4871cd632707741d299e64f1f3087cdc2e
Hichem SERIKET <hichem.seriket@utbm.fr>
2022-05-16 10:29:37 +0200

    fixing push error

---
+++ testGPU.py
@@ -0,0 +1,24 @@
+# import tensorflow as tf
+#
+# with tf.device('/cpu:0'):
+#     a_c = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a-cpu')
+#     b_c = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b-cpu')
+#     c_c = tf.matmul(a_c, b_c, name='c-cpu')
+#
+# with tf.device('/gpu:0'):
+#     a_g = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a-gpu')
+#     b_g = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b-gpu')
+#     c_g = tf.matmul(a_g, b_g, name='c-gpu')
+#
+# with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) as sess:
+#     print(sess.run(c_c))
+#     print(sess.run(c_g))
+#
+# print("DONE!")
+
+
+import tensorflow as tf
+
+tf.test.is_built_with_cuda()
+
+tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None)
